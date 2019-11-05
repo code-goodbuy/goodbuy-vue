00c083483b4e96ad514c93a3e9c214aa577dc779
Patrick <patrick-weber@outlook.com>
2019-11-05 13:36:23 +0100

    adds stylable scanner

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
-    <router-view/>
+    <Home/>
   </div>
 </template>
 
+<script>
+import Home from '@/components/Home'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
 <style>
+body { margin: 0; }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Scanner.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="scanner">
+    <div ref="quagga" class="viewport" />
+  </div>
+</template>
+
+<script>
+import Quagga from 'quagga'
+import VueScreenSize from 'vue-screen-size'
+
+export default {
+  name: 'Scanner',
+  mixins: [VueScreenSize.VueScreenSizeMixin],
+  data() {
+    return {
+      results: []
+    }
+  },
+  watch: {
+    $vssWidth() {
+      this.initScanner()
+    },
+    $vssHeight() {
+      this.initScanner()
+    }
+  },
+  mounted () {
+    this.initScanner()
+  },
+  methods: {
+    initScanner() {
+      this.$nextTick(() => {
+        Quagga.init({
+          locate: false,
+          inputStream : {
+            name : "Live",
+            type : "LiveStream",
+            target: this.$refs.quagga,
+            constraints: {
+              width: this.$vssWidth,
+              height: this.$vssHeight / 2,
+              facingMode: "environment" // or user
+            },
+          },
+          decoder : {
+            readers : ['ean_8_reader'],
+          }
+        },
+        () => this.start())
+      })
+    },
+    start() {
+      Quagga.onDetected(this.onDetected)
+      Quagga.onProcessed(this.onProcessed)
+      Quagga.start()
+      console.log('Quagga started!')
+    },
+    onProcessed() {
+      const drawingCanvas = Quagga.canvas.dom.overlay;
+      drawingCanvas.style.display = 'none';
+    },
+    onDetected(data) {
+      let code = data.codeResult.code
+      console.log(data)
+      if (code.length === 13) {
+        this.results.push(code)
+      }
+      if (this.results.length > 10) {
+        this.$emit('updateBarcode', this.getBarcode())
+        this.results = []
+      }
+    },
+    stop() {
+      Quagga.offDetected(this.onDetected)
+      Quagga.stop()
+      this.$refs.quagga.querySelector('video').remove()
+      this.$refs.quagga.querySelector('canvas').remove()
+    },
+    getBarcode() {
+      return this.findMost(this.results)
+    },
+    findMost(numbers) {
+      let counted = numbers.reduce((acc, curr) => {
+        if (curr in acc) {
+            acc[curr]++;
+        } else {
+            acc[curr] = 1;
+        }
+        return acc;
+      }, {});
+      let mode = Object.keys(counted).reduce((a, b) => counted[a] > counted[b] ? a : b);
+      return mode;
+    }
+  }
+}
+</script>
