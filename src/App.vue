fdbd838183b6cfc93699a7d5c103a1f0dc173b13
Patrick <patrick-weber@outlook.com>
2019-11-20 07:48:30 +0100

    adds warnings if your setup is not correct

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,49 @@
   <div id="app">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
     <transition name="fade" mode="out-in">
-      <router-view />
+      <router-view v-if="isMobile && isVertical"/>
     </transition>
+    <div v-if="!isVertical">This app currently only works horizontally</div>
+    <div v-else-if="!isMobile">This app currently only works on mobile</div>
+    <div v-else-if="!isTooOld">This app currently only works on modern devices</div>
   </div>
 </template>
 
 <script>
+import VueScreenSize from 'vue-screen-size'
+
 export default {
-  name: 'app'
+  name: 'app',
+  mixins: [VueScreenSize.VueScreenSizeMixin],
+  data() {
+    return {
+      isMobile: true,
+      isVertical: true,
+      isTooOld: false,
+    }
+  },
+  watch: {
+    $vssWidth() {
+      this.testWindow()
+    },
+    $vssHeight() {
+      this.testWindow()
+    }
+  },
+  mounted() {
+    this.testWindow()
+  },
+  methods: {
+    testWindow() {
+      if (this.$vssWidth > this.$vssHeight) {
+        this.isVertical = false
+      } else if (this.$vssWidth > 500 || this.$vssHeight > 900) {
+        this.isMobile = false
+      } else if (this.$vssWidth < 320 || this.$vssHeight < 560) {
+        this.isTooOld = true
+      }
+    }
+  }
 }
 </script>
 
