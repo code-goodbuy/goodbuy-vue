ecb400d7888a52f68e96a42741df3c0aa981e423
Anthony <anthony.sherrill@code.berlin>
2020-04-08 10:14:02 +0200

    try to fix view with css

--- src/components/feature/FeatureView.vue
+++ src/components/feature/FeatureView.vue
@@ -17,3 +17,12 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.feature-view {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+}
+</style>
