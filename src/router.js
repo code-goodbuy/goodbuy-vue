ba56c47896ddd60d6ad829ceece9fc648c38d599
Darjusch <Darjusch.schrand@code.berlin>
2020-05-06 19:57:37 +0200

    bundle

--- src/router.js
+++ src/router.js
@@ -2,32 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import IntroView from '@/components/intro/IntroView.vue'
-import ScannerView from '@/components/scanner/ScannerView.vue'
-import ProductView from '@/components/product/ProductView.vue'
-import InstagramView from '@/components/instagram/InstagramView.vue'
-import PageNotFound from '@/fallbacks/PageNotFound.vue'
-import FeatureView from '@/components/feature/FeatureView.vue'
-import TableView from '@/components/table/TableView.vue'
-import Blacklist from '@/components/blacklist/Blacklist.vue'
-import BlacklistTutorial from '@/components/blacklist/BlacklistTutorialIntroduction.vue'
-import Profile from '@/components/profile/Profile.vue'
 import { authGuard } from "./auth/authGuard";
 
 
 Vue.use(Router)
 
 export const routes=[
-    { path: "*", component: PageNotFound },
     { path: '/' , component: IntroView },
-    { path: '/instant-feedback', name:'instant-feedback', component: ScannerView },
-    { path: '/instant-feedback/:code', name:'product', component: ProductView },
-    { path: '/instagram', name: 'instagram', component: InstagramView},
-    { path: '/fridge-karma/result',name: 'result', component: TableView},
-    { path: '/feature', name: 'feature', component: FeatureView},
-    { path: '/fridge-karma', name:'fridge-karma', component: ScannerView},
-    { path: '/blacklist', name:'blacklist', component: Blacklist},
-    { path: '/blacklist-tutorial', name: 'blacklist-tutorial', component: BlacklistTutorial},
-    { path: '/profile', name: 'profile', component: Profile,  beforeEnter: authGuard},
+    { path: '/feature', name: 'feature', component: () => import(/* webpackChunkName: "FeatureView" */ '@/components/feature/FeatureView.vue') },
+    { path: '/instant-feedback', name:'instant-feedback', component: () => import(/* webpackChunkName: "ScannerView" */ '@/components/scanner/ScannerView.vue') },
+    { path: '/instant-feedback/:code', name:'product', component: () => import(/* webpackChunkName: "ProductView" */ '@/components/product/ProductView.vue') },
+    { path: '/fridge-karma', name:'fridge-karma',  component: () => import(/* webpackChunkName: "ScannerView" */ '@/components/scanner/ScannerView.vue') },
+    { path: '/fridge-karma/result',name: 'result', component: () => import(/* webpackChunkName: "TableView" */ '@/components/table/TableView.vue') },
+    { path: '/profile', name: 'profile', component: () => import(/* webpackChunkName: "Profile" */ '@/components/profile/Profile.vue'),  beforeEnter: authGuard },
+    { path: '/blacklist-tutorial', name: 'blacklist-tutorial', component: () => import(/* webpackChunkName: "BlacklistTutorial" */ '@/components/blacklist/BlacklistTutorialIntroduction.vue') },
+    { path: '/blacklist', name:'blacklist', component: () => import(/* webpackChunkName: "Blacklist" */ '@/components/blacklist/Blacklist.vue') },
+    { path: '/instagram', name: 'instagram', component: () => import(/* webpackChunkName: "InstagramView" */ '@/components/instagram/InstagramView.vue') },
+    { path: "*", component: () => import(/* webpackChunkName: "PageNotFound" */ '@/fallbacks/PageNotFound.vue') },
+
 ]
 
 const router = new Router({
